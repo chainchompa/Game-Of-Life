fc5c06fd06714a077d10c9b67ed958c8c322781e
chainchompa <chainchompa@gmail.com>
2018-12-12 15:17:59 -0500

    Updates

---
+++ precache-manifest.e06fa1d8742c0a9d9109c7854b784dbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b75a669126db37438c9",
+    "url": "/Game-Of-Life/static/js/runtime~main.7b75a669.js"
+  },
+  {
+    "revision": "e8c5e5c5f285181ad9ce",
+    "url": "/Game-Of-Life/static/js/main.e8c5e5c5.chunk.js"
+  },
+  {
+    "revision": "43f608b7e1b5161b231c",
+    "url": "/Game-Of-Life/static/js/1.43f608b7.chunk.js"
+  },
+  {
+    "revision": "e8c5e5c5f285181ad9ce",
+    "url": "/Game-Of-Life/static/css/main.3f819915.chunk.css"
+  },
+  {
+    "revision": "0c567c72bb6d2d355cac1463a7edc54d",
+    "url": "/Game-Of-Life/index.html"
+  }
+];
